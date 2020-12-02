2443004344c49240b0d49ee7c93317374ed2f974
dawidkapka <kapkadawid6@gmail.com>
2020-12-02 14:11:14 +0100

    name-changing added

--- views/account-settings.php
+++ views/account-settings.php
@@ -24,8 +24,30 @@
                     <form action="account-settings.php" method="post">
                         <p class="header-text">Account Information</p><br>
                         <img src="../img/user-icon-big.png" alt="user-icon"><br>
-                            <p class="acc-info">Username: <?=$GLOBALS['name'][1]?></p><br>
-                            <p class="acc-info">Email: <?=$GLOBALS['name'][2]?></p><br>
+                        <?php
+                            if (!isset($_POST['change-username'])) {
+                                echo '<form action="home.php" method="post">';
+                                echo '<p class="acc-info">Username:' . $GLOBALS['name'][1] . '</p>';
+                                echo '<input type="submit" value="Change Username" class="post-button" name="change-username">';
+                                echo '<p class="acc-info">Email: ' . $GLOBALS['name'][2] . '</p>';
+                                echo '<input type="submit" value="Change Email" class="post-button" name="register-box">';
+                                echo '</form>';
+                            } else {
+                                echo '<form action="home.php" method="post">';
+                                echo '<p class="acc-info">New Username: ';
+                                echo '<textarea cols="10" rows="1" name="username-changed" class="new-name-input"></textarea>';
+                                echo '</p>';
+                                echo '<input type="submit" value="Save" class="post-button" name="save-username">';
+                                echo '</form>';
+                            }
+                            if (isset($_POST['save-username'])) {
+                                $change_username = $pdo->prepare("UPDATE users SET username = :new_username WHERE id_user = :id_user");
+                                $change_username->execute([':id_user' => $_SESSION['userid'], ':new_username' => $_POST['username-changed']]);
+                                header("Location: account-settings.php");
+                            }
+                        ?>
+
+
                     </form>
                     </div>
                 </div>
